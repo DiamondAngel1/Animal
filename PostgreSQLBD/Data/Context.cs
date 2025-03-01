41f12b24525ba070db016b81a652bf5f74352afe
DiamondAngel1 <tararukolga78@gmail.com>
2025-03-01 21:13:33 +0200

    first commit

---
+++ PostgreSQLBD/Data/Context.cs
@@ -0,0 +1,10 @@
+﻿using PostgreSQLBD.Data.Entitis;
+using Microsoft.EntityFrameworkCore;
+namespace PostgreSQLBD.Data{
+    public class Context : DbContext{
+        public DbSet<UserEntity> Users { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder){
+            optionsBuilder.UseNpgsql("Host=ep-restless-tree-a87x17dk-pooler.eastus2.azure.neon.tech;Database=neondb;Username=neondb_owner;Password=npg_Xw3F4AUMkluV");
+        }
+    }
+}
---
+++ PostgreSQLBD/Data/Entitis/UserEntity.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace PostgreSQLBD.Data.Entitis{
+    [Table("tbl_Users")]
+    public class UserEntity{
+        [Key]
+        public int Id { get; set; }
+
+        [Required, StringLength(50)]
+        public string FName { get; set; } = string.Empty;
+
+        [Required, StringLength(50)]
+        public string LName { get; set; } = string.Empty;
+
+        [StringLength(100)]
+        public string? Email { get; set; } = string.Empty;
+
+        [Required, StringLength(255)]
+        public string? Phone { get; set; } = string.Empty;
+
+        [Required]
+        public DateOnly? Birthday { get; set; }
+    }
+}
